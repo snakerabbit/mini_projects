4b636ec7900766fd0b8d4e9493f5e4c87adfb123
snakerabbit <alison.s.cheng@gmail.com>
2018-02-06 18:22:51 -0800

    integrated mongodb

--- express_practice/hello_world/index.js
+++ express_practice/hello_world/index.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var multer = require('multer');
 var upload = multer();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_db');
 
 
 app.set('view engine', 'pug');
@@ -29,6 +31,39 @@ app.get('/', function(req, res){
 app.post('/', function(req, res){
   console.log(req.body);
   res.send("received your request!");
+});
+
+var personSchema = mongoose.Schema({
+   name: String,
+   age: Number,
+   nationality: String
+});
+var Person = mongoose.model("Person", personSchema);
+
+app.get('/person', function(req, res){
+  res.render('person');
+});
+
+app.post('/person', function(req, res){
+   var personInfo = req.body; //Get the parsed information
+   if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+      res.render('show_message', {
+         message: "Sorry, you provided worng info", type: "error"});
+   } else {
+      var newPerson = new Person({
+         name: personInfo.name,
+         age: personInfo.age,
+         nationality: personInfo.nationality
+      });
+
+      newPerson.save(function(err, Person){
+         if(err)
+            {res.render('show_message', {message: "Database error", type: "error"});}
+         else
+          {  res.render('show_message', {
+               message: "New person added", type: "success", person: personInfo});}
+      });
+   }
 });
 
 // app.get('/first_template', function(req,res){
